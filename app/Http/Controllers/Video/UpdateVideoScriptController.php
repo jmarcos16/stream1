2fb0d270f1d202b8ede1154949d9fe3fb2c1041a
Zé Marcos <jrodrigues@softexpert.com>
2026-03-05 16:12:04 -0300

    feat: altera o retorno do controlador de atualização de script de vídeo para redirecionamento

--- app/Http/Controllers/Video/UpdateVideoScriptController.php
+++ app/Http/Controllers/Video/UpdateVideoScriptController.php
@@ -6,12 +6,12 @@ namespace App\Http\Controllers\Video;
 
 use App\Http\Controllers\Controller;
 use App\Models\Video;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 final class UpdateVideoScriptController extends Controller
 {
-    public function __invoke(Request $request, Video $video): JsonResponse
+    public function __invoke(Request $request, Video $video): RedirectResponse
     {
         $request->validate([
             'script' => 'required|string|max:10000',
@@ -19,6 +19,6 @@ final class UpdateVideoScriptController extends Controller
 
         $video->update(['script' => $request->input('script')]);
 
-        return response()->json(['success' => true]);
+        return back();
     }
 }
